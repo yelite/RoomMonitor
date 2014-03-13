1254ead82aab7cce21476d5010d32b3027576117
yelite <yelite958@gmail.com>
2014-03-13 19:11:31 +0800

    Render blank plot before pulling data Replace js file with new version from official site

--- static/stat.js
+++ static/stat.js
@@ -14,6 +14,15 @@ function zip() {
     });
 }
 
+var pressure_plot;
+var hum_plot;
+var light_plot;
+var temp_plot;
+
+var temp;
+var pressure;
+var light;
+var hum;
 
 $(function () {
     Highcharts.setOptions({
@@ -21,176 +30,186 @@ $(function () {
             useUTC: false
         }
     });
-    $.getJSON('/data', function (data) {
 
-        var credits = {enabled: false};
-        var chart = {
+    var credits = {enabled: false};
+    var navigator = {enabled: false};
+    var rangeSelector = {
+        buttonTheme: { // styles for the buttons
+            fill: 'none',
+            stroke: 'none',
+            'stroke-width': 0,
+            r: 4,
+            style: {
+                color: '#346691',
+                fontWeight: 'bold'
+            },
+            states: {
+                hover: {
+                },
+                select: {
+                    fill: '#346691',
+                    style: {
+                        color: 'white'
+                    }
+                }
+            }
+        },
+
+        buttons: [
+            {
+                type: 'minute',
+                count: 120,
+                text: '2h'
+            },
+            {
+                type: 'minute',
+                count: 480,
+                text: '8h'
+            },
+            {
+                type: 'day',
+                count: 1,
+                text: '1d'
+            },
+            {
+                type: 'day',
+                count: 7,
+                text: '1w'
+            },
+            {
+                type: 'all',
+                text: 'All'
+            }
+        ],
+        inputEnabled: false,
+        selected: 2
+    };
+    var xAxis = {
+        ordinal: false
+    };
+
+    pressure_plot = new Highcharts.StockChart({
+        chart: {
             plotBackgroundColor: '#F5FAFF',
-            borderRadius: 14
-        };
-        var navigator = {enabled: false};
-        var rangeSelector = {
-            buttonTheme: { // styles for the buttons
-                fill: 'none',
-                stroke: 'none',
-                'stroke-width': 0,
-                r: 4,
-                style: {
-                    color: '#346691',
-                    fontWeight: 'bold'
-                },
-                states: {
-                    hover: {
-                    },
-                    select: {
-                        fill: '#346691',
-                        style: {
-                            color: 'white'
-                        }
-                    }
+            borderRadius: 14,
+            renderTo: 'pressure'
+        },
+        credits: credits,
+        navigator: navigator,
+        rangeSelector: rangeSelector,
+        title: {
+            text: 'Pressure (Pa)'
+        },
+        xAxis: xAxis
+    });
+    temp_plot = new Highcharts.StockChart({
+        chart: {
+            plotBackgroundColor: '#F5FAFF',
+            borderRadius: 14,
+            renderTo: 'temp'
+        },
+        credits: credits,
+        navigator: navigator,
+        rangeSelector: rangeSelector,
+        title: {
+            text: 'Temperature (C)'
+        },
+        xAxis: xAxis
+    });
+    light_plot = new Highcharts.StockChart({
+        chart: {
+            plotBackgroundColor: '#F5FAFF',
+            borderRadius: 14,
+            renderTo: 'light'
+        },
+        credits: credits,
+        navigator: navigator,
+        rangeSelector: rangeSelector,
+        title: {
+            text: 'Light'
+        },
+        xAxis: xAxis
+    });
+    hum_plot = new Highcharts.StockChart({
+        chart: {
+            plotBackgroundColor: '#F5FAFF',
+            borderRadius: 14,
+            renderTo: 'hum'
+        },
+        credits: credits,
+        navigator: navigator,
+        rangeSelector: rangeSelector,
+        title: {
+            text: 'Humidity (%)'
+        },
+        xAxis: xAxis
+    });
+
+    // TODO need a compact form
+    pressure_plot.showLoading();
+    temp_plot.showLoading();
+    light_plot.showLoading();
+    hum_plot.showLoading();
+
+    $.getJSON('/data', function (data) {
+        temp = zip(data.timestamp, data.temp);
+        pressure = zip(data.timestamp, data.pressure);
+        light = zip(data.timestamp, data.light_level);
+        hum = zip(data.timestamp, data.hum);
+
+        pressure_plot.hideLoading();
+        pressure_plot.addSeries(
+            {
+                name: 'Indoor',
+                type: 'spline',
+                data: pressure,
+                tooltip: {
+                    valueDecimals: 2
                 }
-            },
+            }
+        );
+        pressure_plot.rangeSelector.render();
 
-            buttons: [
-                {
-                    type: 'minute',
-                    count: 120,
-                    text: '2h'
-                },
-                {
-                    type: 'minute',
-                    count: 480,
-                    text: '8h'
-                },
-                {
-                    type: 'day',
-                    count: 1,
-                    text: '1d'
-                },
-                {
-                    type: 'day',
-                    count: 7,
-                    text: '1w'
-                },
-                {
-                    type: 'all',
-                    text: 'All'
+
+        hum_plot.hideLoading();
+        hum_plot.addSeries(
+            {
+                name: 'Indoor',
+                type: 'spline',
+                data: hum,
+                tooltip: {
+                    valueDecimals: 2
                 }
-            ],
-            inputEnabled: false,
-            selected: 2
-        };
-        var xAxis = {
-            ordinal: false
-        };
-        var temp = zip(data.timestamp, data.temp);
-        var pressure = zip(data.timestamp, data.pressure);
-        var light = zip(data.timestamp, data.light_level);
-        var hum = zip(data.timestamp, data.hum);
-        // Create the chart
-        $('#pressure').highcharts('StockChart', {
-            chart: chart,
+            }
+        );
+        hum_plot.rangeSelector.render();
 
-            credits: credits,
-
-            navigator: navigator,
-
-            rangeSelector: rangeSelector,
-
-            title: {
-                text: 'Pressure (Pa)'
-            },
-
-            series: [
-                {
-                    name: 'Indoor',
-                    data: pressure,
-                    type: 'spline',
-                    tooltip: {
-                        valueDecimals: 2
-                    }
+        temp_plot.hideLoading();
+        temp_plot.addSeries(
+            {
+                name: 'Indoor',
+                type: 'spline',
+                data: temp,
+                tooltip: {
+                    valueDecimals: 2
                 }
-            ],
+            }
+        );
+        temp_plot.rangeSelector.render();
 
-            xAxis: xAxis
-        });
-        $('#temp').highcharts('StockChart', {
-            chart: chart,
-
-            credits: credits,
-
-            navigator: navigator,
-
-            rangeSelector: rangeSelector,
-
-            title: {
-                text: 'Temperature (C)'
-            },
-
-            series: [
-                {
-                    name: 'Indoor',
-                    data: temp,
-                    type: 'spline',
-                    tooltip: {
-                        valueDecimals: 2
-                    }
+        light_plot.hideLoading();
+        light_plot.addSeries(
+            {
+                name: 'Indoor',
+                type: 'spline',
+                data: light,
+                tooltip: {
+                    valueDecimals: 2
                 }
-            ],
+            }
+        );
+        light_plot.rangeSelector.render();
 
-            xAxis: xAxis
-        });
-        $('#light').highcharts('StockChart', {
-            chart: chart,
 
-            credits: credits,
-
-            navigator: navigator,
-
-            rangeSelector: rangeSelector,
-
-            title: {
-                text: 'Light'
-            },
-
-            series: [
-                {
-                    name: 'Indoor',
-                    data: light,
-                    type: 'spline',
-                    tooltip: {
-                        valueDecimals: 2
-                    }
-                }
-            ],
-
-            xAxis: xAxis
-        });
-        $('#hum').highcharts('StockChart', {
-            chart: chart,
-
-            credits: credits,
-
-            navigator: navigator,
-
-            rangeSelector: rangeSelector,
-
-            title: {
-                text: 'Humidity (%)'
-            },
-
-            series: [
-                {
-                    name: 'Indoor',
-                    data: hum,
-                    type: 'spline',
-                    tooltip: {
-                        valueDecimals: 2
-                    }
-                }
-            ],
-
-            xAxis: xAxis
-        });
     });
 });
